97ef6ff96fd1234ed27aad83dc9ca6c7592ff39b
AnwarMestycer <anwarmestycer0@.com>
2024-10-16 15:48:15 +0100

    update message

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -6,7 +6,7 @@ const Login = () => {
     return (
         <div className="h-screen flex justify-center items-center">
         <div className="flex flex-col items-center justify-center gap-4">
-            <h1 className="font-bold text-3xl">Have a nice day, See you again</h1>
+            <h1 className="font-bold text-3xl">If you’re reading this, you owe me a drink for reviewing my code!</h1>
             <Link to="/">
             <button className="border rounded-lg p-2 w-[90px] hover:bg-blue-400">Back</button>
             </Link>
